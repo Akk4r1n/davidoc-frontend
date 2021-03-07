5099832f9805f969003e0b7a589719ab6446b9f0
akk4r1n <davidriewe01@gmail.com>
2021-03-07 18:12:27 +0100

    Point wildcard route to docviewer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DocviewerComponent } from './components/docviewer/docviewer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "**", component: PagenotfoundComponent }
+  { path: "**", component: DocviewerComponent }
 ];
 
 @NgModule({
